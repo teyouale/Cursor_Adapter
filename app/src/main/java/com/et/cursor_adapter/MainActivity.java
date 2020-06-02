e02d9695b514d042d6c1607ec1fabc82f497833e
eyouale <teyouale@gmail.com>
2020-06-03 00:20:24 +0300

    Connecting Fragment and MainActivty

--- app/src/main/java/com/et/cursor_adapter/MainActivity.java
+++ app/src/main/java/com/et/cursor_adapter/MainActivity.java
@@ -3,12 +3,17 @@ package com.et.cursor_adapter;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -19,7 +24,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button);
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                SMSConversationsListFragment fragment = new SMSConversationsListFragment();
+                fragmentManager.beginTransaction().add(R.id.myFrame,fragment).commit();
+                ViewGroup layout = (ViewGroup) button.getParent();
+                layout.removeView(button);
+
+            }
+        });
     }
 
 
